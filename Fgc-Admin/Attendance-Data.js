dc05784a63c842fb1717d869c9f55106a8f6efa7
AolsCare <106975202+AolsCare@users.noreply.github.com>
2022-09-22 20:43:31 +0100

    Update Attendance-Data.js

--- Fgc-Admin/Attendance-Data.js
+++ Fgc-Admin/Attendance-Data.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-analytics.js";
-import * as XLSX from './node_modules/xlsx/xlsx.mjs';
 
 const firebaseConfig = {
     apiKey: "AIzaSyC_b5gP3Y3kNaC674yFoUQ0wwXm21oQ2Lg",
